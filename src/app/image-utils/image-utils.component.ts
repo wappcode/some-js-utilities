68ca171b2a4f427413bc42b57b8fb7ae6e149b72
JJesus <jjesus@iMac-de-JJesus.local>
2021-08-21 09:24:03 -0500

    Actualización angular 12.2.2

--- src/app/image-utils/image-utils.component.ts
+++ src/app/image-utils/image-utils.component.ts
@@ -7,19 +7,19 @@ import { ImageUtilities } from 'projects/jsutil/src/public_api';
   styleUrls: ['./image-utils.component.scss']
 })
 export class ImageUtilsComponent implements OnInit {
-  source: HTMLImageElement;
-  destiny: HTMLImageElement;
+  source?: HTMLImageElement;
+  destiny?: HTMLImageElement;
 
 
   constructor() { }
 
   ngOnInit() {
-    ImageUtilities.load(require('../../assets/4kimage.png')).then(img => {
-      this.source = img;
-      ImageUtilities.scale(this.source, 800).then(imgScaled => {
-        this.destiny = <HTMLImageElement> imgScaled;
-      });
-    });
+    // ImageUtilities.load(require('../../assets/4kimage.png')).then(img => {
+    //   this.source = img;
+    //   ImageUtilities.scale(this.source, 800).then(imgScaled => {
+    //     this.destiny = <HTMLImageElement> imgScaled;
+    //   });
+    // });
 
   }
 
